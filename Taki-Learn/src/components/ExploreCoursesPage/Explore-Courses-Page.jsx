2b9d9504e92949e6b6fc07361cc3c919fdfde178
adedamola-toye <toye.adedamola@gmail.com>
2024-08-31 14:19:47 +0100

    Still on explore courses .....

--- Taki-Learn/src/components/ExploreCoursesPage/Explore-Courses-Page.jsx
+++ Taki-Learn/src/components/ExploreCoursesPage/Explore-Courses-Page.jsx
@@ -1,10 +1,12 @@
 import CourseCard from './CoursesCard'
-
+import Navbar from '../navbar'
 function ExploreCourses(){
     return(
+        <>
+        <Navbar/>
         <div className="explore-courses-page">
              {/* HEADER */}
-
+        
             <div className = "explore-courses-heading">
                 <h1>Explore Courses</h1>
                 <p>Discover a wide range of tech courses tailored to help you master the latest skills in the industry. From web development to data science  to cybersecurity, our courses at Taki Learn are designed to equip you with the tools you need to excel in the tech world</p>
@@ -14,6 +16,8 @@ function ExploreCourses(){
             {/* CARD */}
             <CourseCard/>
         </div>
+        </>
+        
        
     )
 }
