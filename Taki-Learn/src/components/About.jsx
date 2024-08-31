4f1ed6b87351a788cf4b6336f5ac9b12c7a98f46
Anney24 <idayatanimashaun99@gmail.com>
2024-08-31 16:33:36 +0100

    completed About Section

--- Taki-Learn/src/components/About.jsx
+++ Taki-Learn/src/components/About.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import Button from './button'
+import React from "react";
+import Button from "./button";
 
 const About = () => {
   return (
-    <section className='flex hero'>
-      <div className='about-content'>
-        <h3>About Us</h3>
-        <h2>eLearning providing the best opportunities to the students around the glob.</h2>
+    <section className="about-section">
+      <div className="about-content">
+        <h4>About Us</h4>
+        <h2>
+          eLearning providing the best opportunities to the students around the
+          glob.
+        </h2>
         <p>
-        Install our top-rated dropshipping app to your e-commerce site and get access to US Suppliers, AliExpress vendors, and the best dropshipping and custom products. Start selling the right products to the customer base that you know best. We connect you to inventory, so you can focus on creating a catalog of profitable products for your online store.
+          Install our top-rated dropshipping app to your e-commerce site and get
+          access to US Suppliers, AliExpress vendors, and the best dropshipping
+          and custom products. Start selling the right products to the customer
+          base that you know best. We connect you to inventory, so you can focus
+          on creating a catalog of profitable products for your online store.
         </p>
         <p>
-        Install our top-rated dropshipping app to your e-commerce site and get access to US Suppliers, AliExpress vendors, and the best dropshipping and custom products. Start selling the right products to the customer base that you know best. We connect you to inventory, so you can focus on creating a catalog of profitable products for your online store.
+          Install our top-rated dropshipping app to your e-commerce site and get
+          access to US Suppliers, AliExpress vendors, and the best dropshipping
+          and custom products. Start selling the right products to the customer
+          base that you know best. We connect you to inventory, so you can focus
+          on creating a catalog of profitable products for your online store.
         </p>
-        <Button label= "Contact Us" />
+        <Button label="Join Us" />
       </div>
-      <div className='about-image'>
-        <div>
-          box
-        </div>
-        <div>
-          box
+      <div className="about-image">
+        <div className="shadow-box" >
+            <img src="./Rectangle 13.png" alt="" className="image1" />
+            <img src="./Rectangle 14.png" alt="" className="image2" />
         </div>
       </div>
+      
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
