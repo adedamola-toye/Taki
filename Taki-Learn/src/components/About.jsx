7a132d45eba54d1cd60ceabf694c7ef4527ef7c6
Anney24 <idayatanimashaun99@gmail.com>
2024-08-31 13:47:12 +0100

    addedva

---
+++ Taki-Learn/src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+      <div>
+        <h3>About Us</h3>
+        <h2>eLearning providing the best opportunities to the students around the glob.</h2>
+        <p>
+        Install our top-rated dropshipping app to your e-commerce site and get access to US Suppliers, AliExpress vendors, and the best dropshipping and custom products. Start selling the right products to the customer base that you know best. We connect you to inventory, so you can focus on creating a catalog of profitable products for your online store.
+        </p>
+        <p>
+        Install our top-rated dropshipping app to your e-commerce site and get access to US Suppliers, AliExpress vendors, and the best dropshipping and custom products. Start selling the right products to the customer base that you know best. We connect you to inventory, so you can focus on creating a catalog of profitable products for your online store.
+        </p>
+        <button>
+        Contact Us
+        </button>
+
+      </div>
+      <div>
+        <div>
+          box
+        </div>
+        <div>
+          box
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
