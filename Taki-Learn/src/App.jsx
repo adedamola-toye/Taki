3f7edb360ef747195a70374f295127b49f2e425d
serendipity04 <divineobk@gmail.com>
2024-08-31 15:09:57 +0100

    feat: SignInPage

--- Taki-Learn/src/App.jsx
+++ Taki-Learn/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './components/header'
 
 import './App.css'
+import SignInPage from './components/Sign-in/SignInPage'
 //import  ExploreCoursesPage from './components/ExploreCoursesPage/Explore-Courses-Page'
 function App() {
  
