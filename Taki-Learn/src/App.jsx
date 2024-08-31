7330a84cb2fb87fbdfa3e0c417e96f2fe69f5870
adedamola-toye <toye.adedamola@gmail.com>
2024-08-31 13:37:05 +0100

    Course Card with dummy text

--- Taki-Learn/src/App.jsx
+++ Taki-Learn/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import  ExploreCoursesPage from './components/ExploreCoursesPage/Explore-Courses-Page'
 function App() {
  
 
   return (
     <>
       
-      
+      <ExploreCoursesPage/>
     </>
   )
 }
