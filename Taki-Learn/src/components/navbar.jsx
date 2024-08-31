f737df806fc37b02579410796251d155e9b07a64
Kenny-204 <giwatwins@gmail.com>
2024-08-31 13:20:05 +0100

    navbar sha

--- Taki-Learn/src/components/navbar.jsx
+++ Taki-Learn/src/components/navbar.jsx
@@ -4,22 +4,25 @@ return(
     <nav>
         
         <nav class="navbar flex ">
-            <p class="logo">Taki Learn</p>
+            <p class="logo"><b>Taki Learn</b></p>
             <ul class="flex">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">About</a></li>
                 <li><a href="#">All courses</a></li>
             </ul>
+            <div className="nav--buttons">
+
+            <a>Login</a>
             <button>Sign Up</button>
-            <button>S</button>
+            </div>
             <div class="toggle_Btn">
                 <svg fill="#42A7C3" width="50px" height="50px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24"><path d="M20,11H4c-0.6,0-1,0.4-1,1s0.4,1,1,1h16c0.6,0,1-0.4,1-1S20.6,11,20,11z M4,8h16c0.6,0,1-0.4,1-1s-0.4-1-1-1H4C3.4,6,3,6.4,3,7S3.4,8,4,8z M20,16H4c-0.6,0-1,0.4-1,1s0.4,1,1,1h16c0.6,0,1-0.4,1-1S20.6,16,20,16z"/></svg>
             </div>
 
             <div class="dropdown--menu ">
-                <li><a class="dropdown--item" href="#">Product</a></li>
-                <li><a class="dropdown--item" href="#">Contact Us</a></li>
-                <li><a class="dropdown--item" href="#">About Us</a></li>
+                <li><a class="dropdown--item" href="#">Home</a></li>
+                <li><a class="dropdown--item" href="#">About</a></li>
+                <li><a class="dropdown--item" href="#">All courses</a></li>
                 <button class="dropdown--item">Sign Up</button>
             </div>
         </nav>
