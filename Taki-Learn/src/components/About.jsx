4e858dbef2d48271e1ba7b60ed97cdfa57c4b74a
Anney24 <idayatanimashaun99@gmail.com>
2024-08-31 14:23:59 +0100

    adding about page

--- Taki-Learn/src/components/About.jsx
+++ Taki-Learn/src/components/About.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Button from './button'
 
 const About = () => {
   return (
     <section className='about-section'>
-      <div>
+      <div className='about-content'>
         <h3>About Us</h3>
         <h2>eLearning providing the best opportunities to the students around the glob.</h2>
         <p>
@@ -12,12 +13,9 @@ const About = () => {
         <p>
         Install our top-rated dropshipping app to your e-commerce site and get access to US Suppliers, AliExpress vendors, and the best dropshipping and custom products. Start selling the right products to the customer base that you know best. We connect you to inventory, so you can focus on creating a catalog of profitable products for your online store.
         </p>
-        <button>
-        Contact Us
-        </button>
-
+        <Button label= "Contact Us" />
       </div>
-      <div>
+      <div className='about-image'>
         <div>
           box
         </div>
