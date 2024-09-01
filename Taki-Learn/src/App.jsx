cb294e740927b2837f4eb3d4e5bb8573665ffee9
adedamola-toye <toye.adedamola@gmail.com>
2024-09-01 17:06:55 +0100

    fix routing for sign in and sign up pages

--- Taki-Learn/src/App.jsx
+++ Taki-Learn/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import Header from './components/header'
 import About from './components/About'
 import  ExploreCoursesPage from './components/ExploreCoursesPage/Explore-Courses-Page'
+import SignInPage from './components/Sign-in/SignInPage'
+import SignUpPage from './components/Sign-in/SignUpPage'
 function App() {
  
 
@@ -13,6 +15,8 @@ function App() {
         <Route path ="/about" element={<About/>}/>
         <Route path = "/explore-courses" element = {<ExploreCoursesPage/>}/>
         <Route path = "*" element={<Navigate to = "/"/>}/>
+        <Route path = "/signIn" element={<SignInPage/>}/>
+        <Route path = "/signUp"element={<SignUpPage/>}/>
       </Routes>
       {/* <About id = 'about' /> */}
     </BrowserRouter>
