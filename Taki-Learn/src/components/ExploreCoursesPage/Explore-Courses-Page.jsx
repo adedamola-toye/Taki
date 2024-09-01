2dea8fd431373d43bec9d605ac5bb7d006f6d80a
adedamola-toye <toye.adedamola@gmail.com>
2024-09-01 16:21:36 +0100

    done with css for explore courses page

--- Taki-Learn/src/components/ExploreCoursesPage/Explore-Courses-Page.jsx
+++ Taki-Learn/src/components/ExploreCoursesPage/Explore-Courses-Page.jsx
@@ -1,25 +1,34 @@
-import CourseCard from './CoursesCard'
-import Navbar from '../navbar'
-function ExploreCourses(){
-    return(
-        <>
-        <Navbar/>
-        <div className="explore-courses-page">
-             {/* HEADER */}
-        
-            <div className = "explore-courses-heading">
-                <h1>Explore Courses</h1>
-                <p>Discover a wide range of tech courses tailored to help you master the latest skills in the industry. From web development to data science  to cybersecurity, our courses at Taki Learn are designed to equip you with the tools you need to excel in the tech world</p>
-            </div>
-            
+import CourseCard from "./CoursesCard";
+import Navbar from "../navbar";
+function ExploreCourses() {
+  return (
+    <>
+      <Navbar />
+      <div className="explore-courses-page">
+        {/* HEADER */}
 
-            {/* CARD */}
-            <CourseCard/>
+        <div className="explore-courses-heading">
+          <h1>Explore Courses</h1>
+          <p>
+            Discover a wide range of tech courses tailored to help you master
+            the latest skills in the industry. From web development to data
+            science to cybersecurity, our courses at Taki Learn are designed to
+            equip you with the tools you need to excel in the tech world
+          </p>
         </div>
-        </>
-        
-       
-    )
+
+        {/* CARD */}
+        <CourseCard />
+        <div className="quiz-invitation">
+          <p>
+            Not sure the course for you? take this personalized quiz to choose a
+            track
+          </p>
+          <button>Take Quiz</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ExploreCourses
+export default ExploreCourses;
