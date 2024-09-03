de128eb0a6a5d09e93288aea18c52c87cb59be52
Kenny-204 <giwatwins@gmail.com>
2024-09-03 11:52:51 +0100

    ...

--- Taki-Learn/src/App.jsx
+++ Taki-Learn/src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 //import Header from './components/homePage/header'
 import About from './components/homePage/About'
 import  ExploreCoursesPage from './components/ExploreCoursesPage/Explore-Courses-Page'
-import SignInPage from './components/Sign-in/SignInPage'
-import SignUpPage from './components/Sign-in/SignUpPage'
+// import SignInPage from './components/Sign-in/SignInPage'
+// import SignUpPage from './components/Sign-in/SignUpPage'
 import Home from './components/homePage/home';
 import PersonalizedQuiz from './components/Quiz/PersonalizedQuiz'
 function App() {
@@ -16,8 +16,8 @@ function App() {
         <Route path = "/" element={<Home/>}/>
         <Route path ="/about" element={<About/>}/>
         <Route path = "/explore-courses" element = {<ExploreCoursesPage/>}/>
-        <Route path = "/signIn" element={<SignInPage/>}/>
-        <Route path = "/signUp"element={<SignUpPage/>}/>
+        {/* <Route path = "/signIn" element={<SignInPage/>}/>
+        <Route path = "/signUp"element={<SignUpPage/>}/> */}
         <Route path = "/quiz" element={<PersonalizedQuiz/>}/>
        < Route path = "*" element={<Navigate to = "/"/>}/>
       </Routes>
