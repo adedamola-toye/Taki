350e486cc786dcae1054052a673fbe6faa03f37e
adedamola-toye <toye.adedamola@gmail.com>
2024-09-11 16:33:35 +0100

    Course completion status

--- Taki-Learn/src/components/Courses/CoursePage.jsx
+++ Taki-Learn/src/components/Courses/CoursePage.jsx
@@ -1,17 +1,45 @@
 import { useParams } from "react-router-dom";
 import courses from "./coursesData";
 import { Link } from "react-router-dom";
-
+import { useEffect, useState } from "react";
 
 function CoursePage() {
   const { courseName } = useParams();
+  const [completedTopics, setCompletedTopics] = useState({});
   const decodedCourseName = decodeURIComponent(courseName).replace(/-/g, " ");
+  const [completionPercentage, setCompletionPercentage] = useState(0);
 
   const course = courses.find(
-    (course) => course.name && typeof course.name === 'string' &&
-    course.name.toLowerCase() === decodedCourseName?.toLowerCase()
+    (course) =>
+      course.name &&
+      typeof course.name === "string" &&
+      course.name.toLowerCase() === decodedCourseName?.toLowerCase()
   );
-  
+
+  useEffect(() => {
+    if (!course || !course.topics) {
+      return;
+    }
+    const completed = {}; //stores topic and completed status as key value pair
+    let completedCount = 0;
+
+    course?.topics.forEach((topic) => {
+      const completedStatus =
+        localStorage.getItem(`completed-${topic.topicName}`) === "true";
+      completed[topic.topicName] = completedStatus;
+
+      if (completedStatus) {
+        completedCount += 1;
+      }
+    });
+    setCompletedTopics(completed);
+
+    //Calculating completion percentage
+    const completionPercentage = Math.round(
+      (completedCount / course.topics.length) * 100
+    );
+    setCompletionPercentage(completionPercentage)
+  }, [course]);
 
   if (!course) {
     return <p>Course not found</p>;
@@ -26,6 +54,12 @@ function CoursePage() {
       <div className="course-page">
         <h1 className="courseHead">{course.name}</h1>
         <p className="courseDescription">{course.description}</p>
+
+      <div className="course-completion-status">
+        <h2>Overall Progress: {completionPercentage}% Complete</h2>
+        <progress value = {completionPercentage} max = "100"></progress>
+      </div>
+
         <div className="course-topics">
           <h2>Topics</h2>
           <ul className="topics-list">
@@ -38,6 +72,11 @@ function CoursePage() {
                 >
                   {topic.topicName}
                 </Link>
+                {completedTopics[topic.topicName] ? (
+                  <span className="completed-status">Completed</span>
+                ) : (
+                  <span className="not-completed-status">Not Completed</span>
+                )}
               </li>
             ))}
           </ul>
